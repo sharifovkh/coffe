ca3a5da4c9a8e037d02af5b064344cef5e732d69
Sharipov Khushnud <101363105+sharifovkh@users.noreply.github.com>
2022-06-10 17:28:54 +0500

    Add files via upload

---
+++ COFFE/js/main.js
@@ -0,0 +1,44 @@
+
+
+gsap.from('.nav__logo, .nav__toggle',{
+	opacity: 0,
+	duration: 1,
+	delay: 2,
+	y: 10
+})
+
+gsap.from('.nav__item', {
+	opacity: 0,
+	duration: 1,
+	delay: 2.1,
+	y: 30,
+	stagger: 0.2,
+})
+
+gsap.from('.home__title', {
+	opacity: 0,
+	duration: 1,
+	delay: 1.6,
+	y: 30
+})
+
+gsap.from('.home__description', {
+	opacity: 0,
+	duration: 1,
+	delay: 1.8,
+	y:30
+})
+
+gsap.from('.home__button', {
+	opacity: 0,
+	duration: 1,
+	delay: 2.1,
+	y:30
+})
+
+gsap.from('.home__img', {
+	opacity: 0,
+	duration: 1,
+	delay: 1.3,
+	y: 30
+})
